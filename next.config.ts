a99a265078e99d1153a5d86ca19f2efb77479175
FermatD <mnebongo@gmail.com>
2024-12-12 20:41:52 -0500

    added search and pagination functionality

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/**import type { NextConfig } from 'next'?*/
+ 
+const nextConfig = {
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
-
-export default nextConfig;
+ 
+export default nextConfig
